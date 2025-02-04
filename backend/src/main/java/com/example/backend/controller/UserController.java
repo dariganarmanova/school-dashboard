424a31016006fb07c41d7b574de51247036d749f
dariganarmanova <narmanovadariga@gmail.com>
2025-02-04 16:10:44 +0500

    controller for auth

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,5 +1,27 @@
 package com.example.backend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.backend.model.AuthenticationRequest;
+import com.example.backend.model.AuthenticationResponse;
+import com.example.backend.model.RegisterRequest;
+import com.example.backend.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
-    
+    private final UserService userService;
+    @PostMapping("/api/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(userService.register(request));
+    }
+    @PostMapping("/api/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(userService.authenticate(request));
+    }
 }
