b811de53cbedee6e8ddf220be77cbac141f86c32
dariganarmanova <narmanovadariga@gmail.com>
2025-02-04 16:02:26 +0500

    auth service

---
+++ backend/src/main/java/com/example/backend/model/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.example.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String password;
+    private Integer grade;
+    private String head_teacher;
+}
---
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,47 @@
+package com.example.backend.service;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.backend.config.JwtService;
+import com.example.backend.model.AuthenticationRequest;
+import com.example.backend.model.AuthenticationResponse;
+import com.example.backend.model.RegisterRequest;
+import com.example.backend.model.User;
+import com.example.backend.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setFirstname(request.getFirstname());
+        user.setLastname(request.getLastname());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setGrade(request.getGrade());
+        user.setHead_teacher(request.getHead_teacher());
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        AuthenticationResponse authenticationResponse = new AuthenticationResponse();
+        authenticationResponse.setToken(jwtToken);
+        return authenticationResponse;
+    }
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        User user = userRepository.findByEmail(request.getEmail())
+            .orElseThrow(() -> new RuntimeException("User cannot be found"));
+        var jwtToken = jwtService.generateToken(user);
+        AuthenticationResponse authenticationResponse = new AuthenticationResponse();
+        authenticationResponse.setToken(jwtToken);
+        return authenticationResponse;
+    }
+}
